74b446f2982719d3d5f0a97be0b9468ebd2b7d30
Cynthia Njomo <cynthiangoirinjomo@gmail.com>
2022-11-23 12:19:06 +0300

    completed assignment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,16 @@
 class PostsController < ApplicationController
+  rescue_from ActiveRecord::RecordInvalid, with: :render_updated_post_not_valid
 
   def show
-    post = Post.find(params[:id])
+    post = find_post
     
     render json: post
   end
 
   def update
-    post = Post.find(params[:id])
+    post = find_post
 
-    post.update(post_params)
+    post.update!(post_params)
 
     render json: post
   end
@@ -18,6 +19,15 @@ class PostsController < ApplicationController
 
   def post_params
     params.permit(:category, :content, :title)
+  end
+
+  def find_post
+    Post.find(params[:id])
+  end
+
+  def render_updated_post_not_valid (invalid)
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+
   end
 
 end
